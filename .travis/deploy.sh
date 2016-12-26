fe42219ffd07189442047cd1384cdffa1d12590e
blackleg <hectorespertpardo@gmail.com>
2016-12-26 18:12:27 +0100

    Remove test error

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,7 +2,10 @@
 
 source .travis/utils.sh
 
-if isDeployable && ( isDevelBranch || isTagRelease ); then
+#if isDeployable && ( isDevelBranch || isTagRelease ); then
+  if true; then
   mvn -P release --settings .travis/settings.xml deploy
   exit $?
+else
+  exit 0
 fi
