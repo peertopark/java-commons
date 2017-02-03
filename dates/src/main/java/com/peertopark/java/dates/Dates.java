863c5e22792a1be3f7008d234c58e2d4ffb4add1
blackleg <hectorespertpardo@gmail.com>
2017-02-03 22:30:23 +0100

    Add funtion in dates

--- dates/src/main/java/com/peertopark/java/dates/Dates.java
+++ dates/src/main/java/com/peertopark/java/dates/Dates.java
@@ -15,6 +15,7 @@
  */
 package com.peertopark.java.dates;
 
+import com.peertopark.java.commons.utilities.Objects;
 import java.util.Date;
 import org.joda.time.Duration;
 
@@ -23,6 +24,24 @@ import org.joda.time.Duration;
  * @author hector
  */
 public class Dates {
+    
+    public static final int SECONDS_IN_MONTH = 2592000;
+    public static final int SECONDS_IN_WEEK = 604800;
+    public static final int SECONDS_IN_DAY = 86400;
+    public static final int SECONDS_IN_TWO_DAY = 172800;
+
+    public static final int MINUTES_IN_HOUR = 60;
+    public static final int SECONDS_IN_HOUR = 3600;
+
+    public static final int SECONDS_IN_THREE_HOURS = 10800;
+
+    public static final int DAYS_IN_MONTH = 30;
+    
+    public static final int FIVE_MIN_IN_MILLIS = 300000;
+    public static final int THIRTY_MIN_IN_MILLIS = 1800000;
+    public static final int THIRTY_MIN_IN_SECONDS = 1800;
+    
+    public static final int THIRTY_DAYS_IN_SECONDS = 2592000;
     
     
     /**
@@ -43,6 +62,21 @@ public class Dates {
      */
     public static long getSecondsFromMillis(long milliseconds) {
         return Duration.millis(milliseconds).getStandardSeconds();
+    }
+    
+    
+    /**
+     * Transforma un Date en un String con el valor Long del Date.
+     *
+     * @param fecha
+     * @return
+     */
+    public static String transformDateToLongString(Date fecha) {
+        if (Objects.isNull(fecha)) {
+            return null;
+        } else {
+            return String.valueOf(fecha.getTime());
+        }
     }
     
 }
