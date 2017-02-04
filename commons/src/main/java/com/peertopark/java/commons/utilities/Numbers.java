865e595c8122ef112615230d800352c6fccdc103
blackleg <hectorespertpardo@gmail.com>
2017-02-05 00:14:36 +0100

    Move more utilities from blackleg library

--- commons/src/main/java/com/peertopark/java/commons/utilities/Numbers.java
+++ commons/src/main/java/com/peertopark/java/commons/utilities/Numbers.java
@@ -15,19 +15,25 @@
  */
 package com.peertopark.java.commons.utilities;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  *
  * @author hector
  */
 public class Numbers {
-    
-    public static final double ZERODOUBLE = 0.0; 
+
+    public static final double ZERODOUBLE = 0.0;
     public static final int ZERO = 0;
     public static final int ONE = 1;
     public static final int MINUSONE = -1;
     public static final int TWO = 2;
     public static final int MINUSTWO = -2;
-    
+
     public static final int THREE = 3;
     public static final int FOUR = 4;
     public static final int FIVE = 5;
@@ -45,7 +51,7 @@ public class Numbers {
     public static final int SEVENTEEN = 17;
     public static final int EIGHTTEEN = 18;
     public static final int NINETEEN = 19;
-    
+
     public static final int TWENTY = 20;
     public static final int TWENTY_ONE = 21;
     public static final int TWENTY_TWO = 22;
@@ -56,14 +62,136 @@ public class Numbers {
     public static final int TWENTY_SEVEN = 27;
     public static final int TWENTY_EIGHT = 28;
     public static final int TWENTY_NINE = 29;
-    
+
     public static final int THIRTY = 30;
     public static final int THIRTY_ONE = 31;
-    
+
     public static final int SIXTY = 60;
-    
+
     public static final int ONE_HUNDRED = 100;
     public static final double ONE_HUNDRED_DOUBLE = 100.0;
     public static final int ONETHOUSAND = 1000;
-    
+
+    public static DecimalFormat getDecimalFormat() {
+        return new DecimalFormat();
+    }
+
+    public static DecimalFormat getDecimalFormat(String format) {
+        return new DecimalFormat(format);
+    }
+
+    /**
+     * Simple round double to string
+     *
+     * @param number A double number
+     * @return Rounded double string
+     */
+    public static String simpleRoundDoubleToString(double number) {
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
+        numberFormat.setMaximumFractionDigits(TWO);
+        numberFormat.setMinimumFractionDigits(TWO);
+        return numberFormat.format(number);
+    }
+
+    public static BigDecimal getBitDecimal(double number) {
+        return new BigDecimal(number);
+    }
+
+    public static BigDecimal getBigDecimalWithScale(double number, int scale) {
+        return getBitDecimal(number).setScale(scale, RoundingMode.HALF_UP);
+    }
+
+    /**
+     * Simple round double
+     *
+     * @param number A double number
+     * @return Rounded double
+     */
+    public static double simpleRoundDouble(double number) {
+        return simpleRoundDouble(number, TWO);
+    }
+
+    /**
+     * Simple round double
+     *
+     * @param number A double number
+     * @param scale
+     * @return Rounded double
+     */
+    public static double simpleRoundDouble(double number, int scale) {
+        return getBigDecimalWithScale(number, scale).doubleValue();
+    }
+
+    /**
+     * Convierte un double a un int
+     *
+     * @param number
+     * @return
+     */
+    public static int intFromDouble(double number) {
+        return Double.valueOf(number).intValue();
+    }
+
+    /**
+     *
+     * @param number
+     * @return
+     */
+    public static Integer integerFromDouble(double number) {
+        return intFromDouble(number);
+    }
+
+    public static double doubleFromInt(int number) {
+        return Integer.valueOf(number).doubleValue();
+    }
+
+    /**
+     * Round to the next multiple
+     *
+     * @param number
+     * @param multiple
+     * @return
+     */
+    public static int roundToMultiple(double number, int multiple) {
+        return Math.toIntExact(Math.round(number / multiple) * multiple);
+    }
+
+    /**
+     * Round to the next multiple
+     *
+     * @param number
+     * @param multiple
+     * @return
+     */
+    public static int roundToMultiple(int number, int multiple) {
+        return roundToMultiple(doubleFromInt(number), multiple);
+    }
+
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    public static boolean isNegative(double number) {
+        return number < 0;
+    }
+
+    /**
+     * Return if number is higher than number to compare
+     *
+     * @param numberToCompare
+     * @param number
+     * @return True if number is higher than number to compare
+     */
+    public static boolean isHigherThan(Double numberToCompare, Double number) {
+        return number > numberToCompare;
+    }
+
+    public static int toMinus(int number) {
+        if (isNegative(number)) {
+            return number;
+        } else {
+            return -number;
+        }
+    }
+
 }
--- dates/src/main/java/com/peertopark/java/dates/Dates.java
+++ dates/src/main/java/com/peertopark/java/dates/Dates.java
@@ -16,15 +16,21 @@
 package com.peertopark.java.dates;
 
 import com.peertopark.java.commons.utilities.Objects;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
+import org.apache.commons.lang3.ObjectUtils;
+import org.joda.time.DateTime;
 import org.joda.time.Duration;
+import org.joda.time.Interval;
 
 /**
  *
  * @author hector
  */
 public class Dates {
-    
+
     public static final int SECONDS_IN_MONTH = 2592000;
     public static final int SECONDS_IN_WEEK = 604800;
     public static final int SECONDS_IN_DAY = 86400;
@@ -36,14 +42,13 @@ public class Dates {
     public static final int SECONDS_IN_THREE_HOURS = 10800;
 
     public static final int DAYS_IN_MONTH = 30;
-    
+
     public static final int FIVE_MIN_IN_MILLIS = 300000;
     public static final int THIRTY_MIN_IN_MILLIS = 1800000;
     public static final int THIRTY_MIN_IN_SECONDS = 1800;
-    
+
     public static final int THIRTY_DAYS_IN_SECONDS = 2592000;
-    
-    
+
     /**
      * New Date
      *
@@ -53,7 +58,18 @@ public class Dates {
         return new Date();
     }
     
+    public static DateTime convert(Date date) {
+        if (Objects.isNull(date)) {
+            return null;
+        } else {
+            return new DateTime(date);
+        }
+    }
     
+    public static DateTime convert(long time) {
+        return new DateTime(time);
+    }
+
     /**
      * Get seconds from milliseconds
      *
@@ -63,13 +79,12 @@ public class Dates {
     public static long getSecondsFromMillis(long milliseconds) {
         return Duration.millis(milliseconds).getStandardSeconds();
     }
-    
-    
+
     /**
      * Transforma un Date en un String con el valor Long del Date.
      *
-     * @param fecha
-     * @return
+     * @param fecha Date
+     * @return String
      */
     public static String transformDateToLongString(Date fecha) {
         if (Objects.isNull(fecha)) {
@@ -78,5 +93,135 @@ public class Dates {
             return String.valueOf(fecha.getTime());
         }
     }
+
+    public static Date fromStringWithFormat(String format, String stringFecha) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.parse(stringFecha);
+    }
+
+    public static Date fromStringWithFormat(String format, String stringFecha, TimeZone timeZone) throws ParseException {
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat(format);
+        formatoDelTexto.setTimeZone(timeZone);
+        return formatoDelTexto.parse(stringFecha);
+    }
+
+    public static String toStringWithFormat(String format, Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        return formatter.format(date);
+    }
+
+    public static String toStringWithFormat(String format, Date date, TimeZone timeZone) {
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        formatter.setTimeZone(timeZone);
+        return formatter.format(date);
+    }
+
+    /**
+     * All dates not null
+     *
+     * @param dates
+     * @return True if all dates not null
+     */
+    public static boolean allNotNull(Date... dates) {
+        return ObjectUtils.allNotNull((Object[]) dates);
+    }
     
+    
+    /**
+     * All dates not null
+     *
+     * @param dates
+     * @return True if all dates not null
+     */
+    public static boolean allNotNull(DateTime... dates) {
+        return ObjectUtils.allNotNull((Object[]) dates);
+    }
+
+    public static boolean anyNull(Date... dates) {
+        return !allNotNull(dates);
+    }
+    
+    public static boolean anyNull(DateTime... dates) {
+        return !allNotNull(dates);
+    }
+
+    public static boolean checkTwoDatesIfNotNull(Date fromDate, Date untilDate) {
+        return allNotNull(fromDate, untilDate);
+    }
+
+    public static boolean afterOrEquals(Date dateToCompare, Date date) {
+        return afterOrEquals(convert(dateToCompare), convert(date));
+    }
+    
+    public static boolean afterOrEquals(DateTime dateToCompare, DateTime date) {
+        if (allNotNull(dateToCompare, date)) {
+            return date.isAfter(dateToCompare) || date.isEqual(dateToCompare);
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean after(Date dateToCompare, Date date) {
+        return after(convert(dateToCompare), convert(date));
+    }
+    
+    public static boolean after(DateTime dateToCompare, DateTime date) {
+        if (allNotNull(dateToCompare, date)) {
+            return date.isAfter(dateToCompare);
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean beforeOrEquals(Date dateToCompare, Date date) {
+        return beforeOrEquals(convert(dateToCompare), convert(date));
+    }
+    
+    public static boolean beforeOrEquals(DateTime dateToCompare, DateTime date) {
+        if (allNotNull(dateToCompare, date)) {
+            return date.isBefore(dateToCompare) || date.isEqual(dateToCompare);
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean before(Date dateToCompare, Date date) {
+        return before(convert(dateToCompare), convert(date));
+    }
+    
+    public static boolean before(DateTime dateToCompare, DateTime date) {
+        if (allNotNull(dateToCompare, date)) {
+            return date.isBefore(dateToCompare);
+        } else {
+            return false;
+        }
+    }
+
+    public static long getDateInterval(Date fromDate, Date untilDate) {
+        Interval interval = getInterval(fromDate, untilDate);
+        return interval.toDurationMillis();
+    }
+
+    public static long getDateInterval(long fromDate, long untilDate) {
+        Interval interval = getInterval(fromDate, untilDate);
+        return interval.toDurationMillis();
+    }
+    
+    public static Interval getInterval(Date fromDate, Date untilDate) {
+        return new Interval(convert(fromDate), convert(untilDate));
+    }
+    
+    
+    public static Interval getInterval(DateTime fromDate, DateTime untilDate) {
+        return new Interval(fromDate, untilDate);
+    }
+    
+    public static Interval getInterval(long fromDate, long untilDate) {
+        return new Interval(fromDate, untilDate);
+    }
+
+    public static long getSecondsInDateInterval(Date fromDate, Date untilDate) {
+        return getSecondsFromMillis(getDateInterval(fromDate, untilDate));
+    }
+
 }
