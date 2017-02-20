4ac6f3985aeb50f184923d8efd4a96a0ecbb437d
blackleg <hectorespertpardo@gmail.com>
2017-02-20 17:44:27 +0100

    Add SocialProvider enum

---
+++ commons/src/test/java/com/peertopark/java/commons/enums/SocialProviderTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017 Peer to Park.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.peertopark.java.commons.enums;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hector
+ */
+public class SocialProviderTest {
+    
+    public SocialProviderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+
+    /**
+     * Test of valueOf method, of class SocialProvider.
+     */
+    @Test
+    public void testValueOf() {
+        String name = "FACEBOOK";
+        SocialProvider expResult = SocialProvider.FACEBOOK;
+        SocialProvider result = SocialProvider.valueOf(name);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of fromString method, of class SocialProvider.
+     */
+    @Test
+    public void testFromString() {
+        String value = "facebook";
+        SocialProvider expResult = SocialProvider.FACEBOOK;
+        SocialProvider result = SocialProvider.fromString(value);
+        assertEquals(expResult, result);
+        
+        value = "FACEBOOK";
+        expResult = SocialProvider.FACEBOOK;
+        result = SocialProvider.fromString(value);
+        assertEquals(expResult, result);
+    }
+
+    
+}
